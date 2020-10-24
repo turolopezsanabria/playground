5e641610182df7b460e908ce6384deafa7aa23ed
Arturo López Sanabria <arturo.lopez@adevinta.com>
2020-10-24 19:39:00 +0200

    playing with sui components

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import Button from '@s-ui/react-atom-button'
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button>Hello</Button>
+      </div>
+    )
+  }
+}
