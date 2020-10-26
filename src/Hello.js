8d79e9cd3596f3450c26cade65ab815b5e6ddc6d
Arturo López Sanabria <arturo.lopez@adevinta.com>
2020-10-26 11:15:56 +0100

    add sui theme

--- src/Hello.js
+++ src/Hello.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react'
+import './index.scss'
 import Button from '@s-ui/react-atom-button'
 
 export default class Hello extends Component {
   render() {
     return (
       <div className="App">
+        <p>
+          The background is grey just to make sure I know the SCSS file is
+          loading
+        </p>
         <Button>Hello</Button>
       </div>
     )
