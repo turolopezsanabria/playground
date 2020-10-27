b8a6caf84d49d06921b058202ea4074062eda5b3
Arturo López Sanabria <arturo.lopez@adevinta.com>
2020-10-27 14:28:55 +0100

    working now

--- src/App.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Hello from './Hello'
-
-const render = Component =>
-  ReactDOM.render(<Component />, document.getElementById('root'))
-
-render(Hello)
