fb7b8ecc4c93844b2ab5d697a883296b0dbd9c0b
Arturo López Sanabria <arturo.lopez@adevinta.com>
2020-11-20 13:00:02 +0100

    add the Round2

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,10 @@ export default function Home() {
       <Head>
         <title>turo's playground</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
       </Head>
       <main className="home">
         <nav className="home-navigation">
