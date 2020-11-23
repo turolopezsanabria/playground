24c447684ffac54f9f8ae75d2166b77195be54fd
Arturo López Sanabria <arturo.lopez@adevinta.com>
2020-11-23 20:58:34 +0100

    adjust performance not success

--- pages/navBar-Mobile-First-Round2/index copy.js
+++ pages/navBar-Mobile-First-Round2/index copy.js
@@ -1,4 +1,4 @@
-import React, {useState, useMemo, Fragment} from 'react'
+import React, {useState, useMemo} from 'react'
 import useOnclickOutside from 'react-cool-onclickoutside'
 import styles from './drawervertical.module.scss'
 import Button from '@s-ui/react-atom-button'
@@ -6,230 +6,113 @@ import AtomInput from '@s-ui/react-atom-input'
 import MoleculeAvatar, {AVATAR_SIZES} from '@s-ui/react-molecule-avatar'
 import Link from 'next/link'
 
-function ButtonForLoggedSuccessfully() {
-  console.log('ButtonForLoggedSuccessfully')
-  return (
-    <Button
-      tabIndex={0}
-      color="primary"
-      design="solid"
-      fullWidth
-      className={'sui-AtomButton--empty' + ' ' + styles.seachbtn}
-      isLoading
-      isSubmit
-    >
-      Log-in
-    </Button>
-  )
-}
-
 export default function Dropdown() {
   const [CallToActionState, setCallToActionState] = useState(
     styles.arrowPointingDown
   )
-
   const [layerState, setLayerState] = useState()
   const [disabled, setDisabled] = useState(true)
-  const [CTAToggleActive, setCTAToggleActive] = useState(false)
+  const [CTAToggleActive, setCTAToggleActive] = useState()
   const [LoggedStatus, setLoggedStatus] = useState(true)
   const [subMenuVisible, setsubMenuVisible] = useState(false)
   const [whichSubmenu, setwhichSubmenu] = useState()
   const [UserIsAttemptinglog, setUserIsAttemptinglog] = useState(false)
 
-  const Initial = useMemo(() => {
-    // ok console.log('Initial')
-    return () => {
-      return (
-        <>
-          <nav
-            aria-labelledby="navigation menu"
-            className={styles.navigation_menu}
-          >
-            <ul>
-              {/* <li onClick={doLogOut}> */}
-              <li onClick={goToMyAccount}>
-                <MoleculeAvatar
-                  size={AVATAR_SIZES.SMALL}
-                  name="John Maplethorp"
-                  src="img/user-2.png"
-                />
-                <span className={styles.label}>john.maplethorp@gmail.com</span>
-                <img
-                  className={styles.nav_arrow}
-                  alt="navigation arrow"
-                  src="img/chevron_right.svg"
-                />
-              </li>
-              <li>
-                <img alt="icon burguer-menu" src="img/anuncios.svg" />
-                <span className={styles.label}>Mis anuncios</span>
-              </li>
-              <li>
-                <img alt="icon burguer-menu" src="img/messages.svg" />
-                <span className={styles.label}>Mis mensajes</span>
-              </li>
-              <li>
-                <img alt="icon burguer-menu" src="img/heart.svg" />
-                <span className={styles.label}>Mis favoritos</span>
-              </li>
-              <li>
-                <img alt="icon burguer-menu" src="img/bell.svg" />
-                <span className={styles.label}>Mis búsquedas</span>
-              </li>
-              <li onClick={goToMyCredits}>
-                <img alt="icon burguer-menu" src="img/credits.svg" />
-                <span className={styles.label}>Mis creditos</span>
-                <img
-                  className={styles.nav_arrow}
-                  alt="navigation arrow"
-                  src="img/chevron_right.svg"
-                />
-              </li>
-              <hr className={styles.menu_divisor} />
-              <li>
-                <img alt="icon burguer-menu" src="img/destacar.svg" />
-                <span className={styles.label}>Destacar anuncios</span>
-              </li>
-              <li>
-                <img alt="icon burguer-menu" src="img/MA_express.svg" />
-                <span className={styles.label}>Milanuncios Express</span>
-              </li>
-              <li>
-                <img alt="icon burguer-menu" src="img/help.svg" />
-                <span className={styles.label}>Ayuda</span>
-              </li>
-            </ul>
-          </nav>
-        </>
-      )
-    }
-  }, [])
-
-  const NavigationMenu = () => {
-    // OK to be called everytimeconsole.log('NavigationMenu')
-    return LoggedStatus ? NavigationMenuLoggedIn : NavigationMenuLoggedOut
-  }
-
   function ClickCTAToggleActive() {
-    // OK console.log('ClickCTAToggleActive')
+    // superok
     return CTAToggleActive ? Closed() : Opended()
   }
 
-  const ButtonOnEmptyState = () => {
-    console.log('emptyStatex2')
-    return useMemo(() => {
-      console.log('emptyStatex3')
-      return (
-        <>
-          <Button
-            tabIndex={0}
-            color="primary"
-            design="solid"
-            fullWidth
-            isSubmit
-            className={'sui-AtomButton--empty' + ' ' + styles.seachbtn}
-            onClick={doLogIn}
-          >
-            Inicia sesión
-          </Button>
-        </>
-      )
-    }, [])
+  function TheLoginButton() {
+    console.log('TheLoginButton')
+    return UserIsAttemptinglog ? success() : emptyState()
   }
 
-  const TheLoginButton = useMemo(() => {
-    // ok console.log('TheLoginButton')
-    return UserIsAttemptinglog
-      ? ButtonForLoggedSuccessfully
-      : ButtonOnEmptyState
-  }, [UserIsAttemptinglog])
-
   const backFromSubMenu = () => {
-    // OK console.log('backFromSubMenu')
     setsubMenuVisible(false)
   }
   const goToMyAccount = () => {
-    // OK console.log('goToMyAccount')
     setsubMenuVisible(true)
     setwhichSubmenu(1)
   }
   const goToMyCredits = () => {
-    // OK console.log('goToMyCredits')
     setsubMenuVisible(true)
     setwhichSubmenu(0)
   }
 
   const subMenu = () => {
-    // OK console.log('subMenu')
     return whichSubmenu ? myAccount() : myCredits()
   }
-  const NavigationMenuLoggedIn = useMemo(() => {
-    console.log('NavigationMenuLoggedIn')
+  const NavigationMenuLoggedIn = () => {
+    /// ///////////////////// this one is called 4 times on REF console.log('NavigationMenuLoggedIn')
     return subMenuVisible ? subMenu() : Initial()
-  }, [subMenuVisible])
+  }
 
-  function doLogOut() {
-    // OK console.log('dologout')
+  const doLogOut = () => {
     setLayerState(styles.layerdrawerVerticalOff)
-
     setTimeout(() => {
       setLoggedStatus(false)
       setCallToActionState(styles.arrowPointingDown)
       setDisabled(true)
+
       setCTAToggleActive(false)
+      setsubMenuVisible(false)
       setUserIsAttemptinglog(false)
     }, 300)
+    // not being called evertime console.log('dologout')
   }
 
-  function doLogIn() {
-    // OK console.log('doLogIn')
+  const doLogIn = () => {
     setUserIsAttemptinglog(true)
     setTimeout(() => {
       setLayerState(styles.layerdrawerVerticalOff)
       setCallToActionState(styles.arrowPointingDown)
-      setsubMenuVisible(false)
-      setDisabled(true)
+      setDisabled(false)
       setCTAToggleActive(false)
     }, 500)
     setTimeout(() => {
       setLoggedStatus(true)
     }, 800)
+    // not being called evertime console.log('dologin')
   }
 
   const Closed = () => {
-    // OK console.log('closed')
     setLayerState(styles.layerdrawerVerticalOff)
     setCallToActionState(styles.arrowPointingDown)
     setDisabled(!disabled)
     setCTAToggleActive(prevValue => !prevValue)
+    setsubMenuVisible(false)
+    // not being called evertime console.log('closed')
   }
 
   const Opended = () => {
-    // OK console.log('opened')
     setLayerState(styles.layerdrawerVerticalOn)
     setCallToActionState(styles.CTAinvisible)
     setDisabled(!disabled)
     setCTAToggleActive(true)
-    setsubMenuVisible(false)
+    // not being called evertime console.log('opened')
   }
 
   const ref = useOnclickOutside(
     () => {
-      // OK console.log('ref')
       setLayerState(styles.layerdrawerVerticalOff)
       setCallToActionState(styles.arrowPointingDown)
       setDisabled(!disabled)
       setCTAToggleActive(prevValue => !prevValue)
+      setsubMenuVisible(false)
     },
     {disabled}
   )
 
-  const ActionsArea = useMemo(() => {
-    return LoggedStatus ? loggedIn : loggedOut
-  }, [CTAToggleActive, LoggedStatus])
+  const ActionsArea = () => {
+    return LoggedStatus ? loggedIn() : loggedOut()
+  }
 
-  function loggedIn() {
-    console.log('functionloggedIn')
+  const NavigationMenu = () => {
+    return LoggedStatus ? NavigationMenuLoggedIn() : NavigationMenuLoggedOut()
+  }
+
+  const loggedIn = () => {
     return (
       <div className={styles.actionsArea + ' ' + CallToActionState}>
         <Button
@@ -238,7 +121,7 @@ export default function Dropdown() {
           design="flat"
           className={'sui-AtomButton--empty' + ' ' + styles.seachbtn}
         >
-          <img alt="icon burguer-menu" src="img/search.svg" />
+          <img alt="icon burguer-menu" src="img/messages.svg" />
         </Button>
         <Button
           tabIndex={0}
@@ -246,7 +129,7 @@ export default function Dropdown() {
           design="flat"
           className={'sui-AtomButton--empty' + ' ' + styles.seachbtn}
         >
-          <img alt="icon burguer-menu" src="img/messages.svg" />
+          <img alt="icon burguer-menu" src="img/search.svg" />
         </Button>
         <button tabIndex={0} className={styles.newActionButton}>
           <img alt="icon burguer-menu" src="img/PTA_White24px.svg" />
@@ -256,7 +139,6 @@ export default function Dropdown() {
         <div
           tabIndex={0}
           className={styles.buttonCTA}
-          role="button"
           onClick={ClickCTAToggleActive}
         >
           <MoleculeAvatar
@@ -277,7 +159,7 @@ export default function Dropdown() {
     )
   }
 
-  function myCredits() {
+  const myCredits = () => {
     return (
       <>
         <nav
@@ -317,7 +199,7 @@ export default function Dropdown() {
     )
   }
 
-  function myAccount() {
+  const myAccount = () => {
     return (
       <>
         <nav
@@ -357,74 +239,80 @@ export default function Dropdown() {
     )
   }
 
-  // function Initialz() {
-  //   return (
-  //     <>
-  //       <nav
-  //         aria-labelledby="navigation menu"
-  //         className={styles.navigation_menu}
-  //       >
-  //         <ul>
-  //           {/* <li onClick={doLogOut}> */}
-  //           <li onClick={goToMyAccount}>
-  //             <MoleculeAvatar
-  //               size={AVATAR_SIZES.SMALL}
-  //               name="John Maplethorp"
-  //               src="img/user-2.png"
-  //             />
-  //             <span className={styles.label}>john.maplethorp@gmail.com</span>
-  //             <img
-  //               className={styles.nav_arrow}
-  //               alt="navigation arrow"
-  //               src="img/chevron_right.svg"
-  //             />
-  //           </li>
-  //           <li>
-  //             <img alt="icon burguer-menu" src="img/anuncios.svg" />
-  //             <span className={styles.label}>Mis anuncios</span>
-  //           </li>
-  //           <li>
-  //             <img alt="icon burguer-menu" src="img/messages.svg" />
-  //             <span className={styles.label}>Mis mensajes</span>
-  //           </li>
-  //           <li>
-  //             <img alt="icon burguer-menu" src="img/heart.svg" />
-  //             <span className={styles.label}>Mis favoritos</span>
-  //           </li>
-  //           <li>
-  //             <img alt="icon burguer-menu" src="img/bell.svg" />
-  //             <span className={styles.label}>Mis búsquedas</span>
-  //           </li>
-  //           <li onClick={goToMyCredits}>
-  //             <img alt="icon burguer-menu" src="img/credits.svg" />
-  //             <span className={styles.label}>Mis creditos</span>
-  //             <img
-  //               className={styles.nav_arrow}
-  //               alt="navigation arrow"
-  //               src="img/chevron_right.svg"
-  //             />
-  //           </li>
-  //           <hr className={styles.menu_divisor} />
-  //           <li>
-  //             <img alt="icon burguer-menu" src="img/destacar.svg" />
-  //             <span className={styles.label}>Destacar anuncios</span>
-  //           </li>
-  //           <li>
-  //             <img alt="icon burguer-menu" src="img/MA_express.svg" />
-  //             <span className={styles.label}>Milanuncios Express</span>
-  //           </li>
-  //           <li>
-  //             <img alt="icon burguer-menu" src="img/help.svg" />
-  //             <span className={styles.label}>Ayuda</span>
-  //           </li>
-  //         </ul>
-  //       </nav>
-  //     </>
-  //   )
-  // }
+  // const Whatever = useMemo(() => {
+  //   return Initial()
+  // }, [])
 
-  const NavigationMenuLoggedOut = useMemo(() => {
-    // ok console.log('NavigationMenuLoggedOut')
+  function Initial() {
+    // not being called evertime
+
+    return (
+      <>
+        <nav
+          aria-labelledby="navigation menu"
+          className={styles.navigation_menu}
+        >
+          <ul>
+            {/* <li onClick={doLogOut}> */}
+            <li onClick={goToMyAccount}>
+              <MoleculeAvatar
+                size={AVATAR_SIZES.SMALL}
+                name="John Maplethorp"
+                src="img/user-2.png"
+              />
+              <span className={styles.label}>john.maplethorp@gmail.com</span>
+              <img
+                className={styles.nav_arrow}
+                alt="navigation arrow"
+                src="img/chevron_right.svg"
+              />
+            </li>
+            <li>
+              <img alt="icon burguer-menu" src="img/anuncios.svg" />
+              <span className={styles.label}>Mis anuncios</span>
+            </li>
+            <li>
+              <img alt="icon burguer-menu" src="img/messages.svg" />
+              <span className={styles.label}>Mis mensajes</span>
+            </li>
+            <li>
+              <img alt="icon burguer-menu" src="img/heart.svg" />
+              <span className={styles.label}>Mis favoritos</span>
+            </li>
+            <li>
+              <img alt="icon burguer-menu" src="img/bell.svg" />
+              <span className={styles.label}>Mis búsquedas</span>
+            </li>
+            <li onClick={goToMyCredits}>
+              <img alt="icon burguer-menu" src="img/credits.svg" />
+              <span className={styles.label}>Mis creditos</span>
+              <img
+                className={styles.nav_arrow}
+                alt="navigation arrow"
+                src="img/chevron_right.svg"
+              />
+            </li>
+            <hr className={styles.menu_divisor} />
+            <li>
+              <img alt="icon burguer-menu" src="img/destacar.svg" />
+              <span className={styles.label}>Destacar anuncios</span>
+            </li>
+            <li>
+              <img alt="icon burguer-menu" src="img/MA_express.svg" />
+              <span className={styles.label}>Milanuncios Express</span>
+            </li>
+            <li>
+              <img alt="icon burguer-menu" src="img/help.svg" />
+              <span className={styles.label}>Ayuda</span>
+            </li>
+          </ul>
+        </nav>
+      </>
+    )
+  }
+
+  function NavigationMenuLoggedOut() {
+    /// ///////////////////// this one is called a lot console.log('NavigationMenuLoggedOut')
     return (
       <>
         <nav
@@ -466,11 +354,43 @@ export default function Dropdown() {
         </nav>
       </>
     )
-  }, [UserIsAttemptinglog])
+  }
 
-  function loggedOut() {
-    /// ///////////////////// this one is called 4 times on REF
-    console.log('functionloggedOut')
+  const emptyState = () => {
+    /// ///////////////////// this one is called 4 times console.log('emptyState')
+    return (
+      <Button
+        tabIndex={0}
+        color="primary"
+        design="solid"
+        fullWidth
+        isSubmit
+        className={'sui-AtomButton--empty' + ' ' + styles.seachbtn}
+        onClick={doLogIn}
+      >
+        Inicia sesión
+      </Button>
+    )
+  }
+  const success = () => {
+    /// ///////////////////// this one is called 4 times
+    //    console.log('success')
+    return (
+      <Button
+        tabIndex={0}
+        color="primary"
+        design="solid"
+        fullWidth
+        className={'sui-AtomButton--empty' + ' ' + styles.seachbtn}
+        isLoading
+        isSubmit
+      >
+        Log-in
+      </Button>
+    )
+  }
+  const loggedOut = () => {
+    /// ///////////////////// this one is called 4 times on REF console.log('loggedOut')
     return (
       <div className={styles.actionsArea + ' ' + CallToActionState}>
         <Button
