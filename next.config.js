f4cef7f6a3b3004086775ae781ac97866b1e12f2
Arturo López Sanabria <arturo.lopez@adevinta.com>
2021-01-21 13:03:37 +0100

    modals

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withTM = require('next-transpile-modules')([
   '@s-ui/react-atom-icon',
   '@s-ui/react-atom-popover',
   '@s-ui/react-molecule-avatar',
-  '@adv-ui/fcs-theme',
+  '@adv-ui/fc-theme',
   '@s-ui/react-hooks',
   '@s-ui/hoc',
   'react-cool-onclickoutside',
